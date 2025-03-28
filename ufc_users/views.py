d58d7e69c208a30c678829233e8b9c4a535f4016
ajay2111 <ajay.kumar2111@yahoo.co.in>
2025-03-28 23:49:40 +0530

    backend changes

--- ufc_users/views.py
+++ ufc_users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .seralizers import RoomSeralizers
+from rest_framework import generics
+from .models import Room
 # Create your views here.
+
+
+class RoomView(generics.CreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSeralizers
